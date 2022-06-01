94a71e42452fa4928063b270fbf2a597b45c4aea
Eric Costa <eric3bcosta@gmail.com>
2022-05-31 22:48:07 -0400

    Primeiro commit do projeto

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Luffy',
+      email: 'luffy2078@luffy.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/12/17/20/50/luffy-6877426_960_720.png'),
+};
